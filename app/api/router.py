9708d0c65686e0e5316705a86ffa46a26bb81237
SAA-DIPESH <dipesh.dhote@saa.ai>
2026-07-16 18:40:16 +0530

    fix code structure in route file.

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from app.api.routes.evaluation_criteria_route import router as evaluation_criteria_router
-
 from app.agents.taxonomy_agent.app.api.routes.router import router as taxonomy_router
 from app.agents.GrammerAI.app.api.router.routes import router as GrammerAI
 from app.api.routes.agent_get_requirements_route import router as agent_requirements_router
@@ -14,6 +13,5 @@ api_router.include_router(agent_requirements_router)
 
 # Evalution Criteria Agent Route
 api_router.include_router(evaluation_criteria_router)
-
 api_router.include_router(taxonomy_router)
 api_router.include_router(GrammerAI)
