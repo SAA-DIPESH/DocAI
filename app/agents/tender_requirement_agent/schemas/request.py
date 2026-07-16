ffffc2f8b968f2aa47d934ccadf0632ba8c49423
SAA-DIPESH <dipesh.dhote@saa.ai>
2026-07-16 21:23:45 +0530

    update agent code

--- app/agents/tender_requirement_agent/schemas/request.py
+++ app/agents/tender_requirement_agent/schemas/request.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field, ConfigDict
 class RequirementRequest(BaseModel):
     company_id: str = Field(alias="CompanyId")
     tender_id: str = Field(alias="TenderId")
-    IsRegenerate: str = Field(alias="IsRegenerate")
+    IsRegenerate: bool 
     UserId: str
     UserName: str
     ProjectId: str
