b9e7936b31d20f75de9943cfa75124b584cf8a68
SAA-DIPESH <dipesh.dhote@saa.ai>
2026-07-20 19:48:39 +0530

    Update load_llms.py

--- app/infrastructure/load_llms.py
+++ app/infrastructure/load_llms.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from app.infrastructure.secrets import secret_manager
 from bson import ObjectId
 import os
-from DocAI.app.infrastructure.token_usage_logger import TokenUsageCallback
+from app.infrastructure.token_usage_logger import TokenUsageCallback
 
 
 load_dotenv()
